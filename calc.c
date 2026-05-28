03b0ec847633eaf4e6594542775394563b8fec25
KomCat23 <artemkurnasenko2010@gmail.com>
2026-05-28 13:16:22 +0300

    Add files via upload

--- calc.c
+++ calc.c
@@ -19,129 +19,154 @@ int main(){
     {
         long f = a+b;
         printf("%ld\n", f);
+        system("pause");
     }
     else if(strcmp(c, "-") == 0 || (strcmp(c, "min") == 0) || (strcmp(c, "minus") == 0))
     {
         long f = a-b;
         printf("%ld\n", f);
+        system("pause");
     }
     else if(strcmp(c, "*") == 0 || (strcmp(c, "mul") == 0) || (strcmp(c, "multiple") == 0))
     {
         long f = a*b;
         printf("%ld\n", f);
+        system("pause");
     }
     else if(strcmp(c, "**") == 0 || (strcmp(c, "exp") == 0) || (strcmp(c, "pow") == 0))
     {
         long f = pow(a,b);
         printf("%ld\n", f);
+        system("pause");
     }
     else if(strcmp(c, "/") == 0 || (strcmp(c, "div") == 0) || (strcmp(c, "division") == 0))
     {
         double f = a/b;
         printf("%lf\n", f);
+        system("pause");
     }
     else if(strcmp(c, "%") == 0)
     {
         long f = a%b;
         printf("%ld\n", f);
+        system("pause");
     }
     else if(strcmp(c, "log") == 0)
     {
         double f = log(a);
         double g = log(b);
         printf("%lf and %lf\n", f,g);
+        system("pause");
     }
     else if(strcmp(c, "log+sum") == 0)
     {
         double f = log(a)+log(b);
         printf("%lf\n", f);
+        system("pause");
     }
     else if(strcmp(c, "log+min") == 0)
     {
         double f = log(a)-log(b);
         printf("%lf\n", f);
+        system("pause");
     }
     else if(strcmp(c, "log+mul") == 0)
     {
         double f = log(a)*log(b);
         printf("%lf\n", f);
+        system("pause");
     }
     else if(strcmp(c, "log+div") == 0)
     {
         double f = log(a)/log(b);
         printf("%lf\n", f);
+        system("pause");
     }
     else if(strcmp(c, "log+pow") == 0 || (strcmp(c, "log+exp") == 0))
     {
         double f = pow(log(a),log(b));
         printf("%lf\n", f);
+        system("pause");
     }
     else if(strcmp(c, "sqrt") == 0)        
     {
         double f = sqrt(a);
         double g = sqrt(b);
         printf("%lf and %lf\n", f,g);
+        system("pause");
     }
     else if(strcmp(c, "sqrt+sum") == 0)
     {
         double f = sqrt(a)+sqrt(b);
         printf("%lf\n", f);
+        system("pause");
     }
     else if(strcmp(c, "sqrt+min") == 0)
     {
     	double f = sqrt(a)-sqrt(b);
         printf("%lf\n", f);
+        system("pause");
     }
     else if(strcmp(c, "sqrt+mul") == 0)
     {
     	double f = sqrt(a)*sqrt(b);
         printf("%lf\n", f);
+        system("pause");
     }
-    else if(strcmp(c, "sqrt+div") == 0)
+    else if(strcmp(c, "sqrt+div") == 0 || (strcmp(c, "sqrt+division") == 0))
     {
     	double f = sqrt(a)/sqrt(b);
         printf("%lf\n", f);
+        system("pause");
     }
     else if(strcmp(c, "sqrt+pow") == 0 || (strcmp(c, "sqrt+exp") == 0))
     {
     	double f = pow(sqrt(a),sqrt(b));
         printf("%lf\n", f);
+        system("pause");
     }
     else if(strcmp(c, "cbrt") == 0)             
     {                                           
         double f = cbrt(a);
         double g = cbrt(b);
         printf("%lf and %lf\n", f,g);
+        system("pause");
     }
     else if(strcmp(c, "cbrt+sum") == 0)
     {
         double f = cbrt(a)+cbrt(b);
         printf("%lf\n", f);
+        system("pause");
     }
     else if(strcmp(c, "cbrt+min") == 0)
     {
         double f = cbrt(a)-cbrt(b);
         printf("%lf\n", f);
+        system("pause");
     }
     else if(strcmp(c, "cbrt+mul") == 0)
     {
         double f = cbrt(a)*cbrt(b);
         printf("%lf\n", f);
+        system("pause");
     }
     else if(strcmp(c, "cbrt+div") == 0)
     {
         double f = cbrt(a)/cbrt(b);
         printf("%lf\n", f);
+        system("pause");
     }
     else if(strcmp(c, "cbrt+pow") == 0 || (strcmp(c, "cbrt+exp") == 0))
     {
         double f = pow(cbrt(a),cbrt(b));
         printf("%lf\n", f);
+        system("pause");
     }
     else
     {
         printf("err\n");
         return 1;
+        system("pause");
     }
     return 0;
 }
